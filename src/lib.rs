c6d7a1a627b508227a9d8bbe30c240c4b8ea0a35
Chris Martinez <sydefekt@hotmail.com>
2023-11-22 20:55:54 -0800

    Doc all features

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 #![doc = include_str!("../README.md")]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
+/// Represents the type alias for an options reference.
 #[cfg(not(all(feature = "di", feature = "async")))]
 pub type Ref<T> = std::rc::Rc<T>;
 
+/// Represents the type alias for an options reference.
 #[cfg(all(not(feature = "di"), feature = "async"))]
 pub type Ref<T> = std::sync::Arc<T>;
 
+/// Represents the type alias for an options reference.
 #[cfg(all(feature = "di", feature = "async"))]
 pub type Ref<T> = di::Ref<T>;
 
@@ -39,6 +43,7 @@ pub use token::*;
 pub use validate::*;
 
 #[cfg(feature = "di")]
+#[cfg_attr(docsrs, doc(cfg(feature = "di")))]
 pub use builder::*;
 
 /// Contains options extension methods.
@@ -47,8 +52,10 @@ pub mod ext {
     use super::*;
     
     #[cfg(feature = "di")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "di")))]
     pub use di_ext::*;
 
     #[cfg(feature = "cfg")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "cfg")))]
     pub use cfg_ext::*;
 }
