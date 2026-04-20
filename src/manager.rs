9402f2357935e5dcefa4f7f375c33688dac947d3
Chris Martinez <sydefekt@hotmail.com>
2026-04-20 15:05:52 -0700

    Apply formatting

--- src/manager.rs
+++ src/manager.rs
@@ -1,6 +1,4 @@
-use crate::{
-    Options, OptionsCache, OptionsFactory, OptionsMonitorCache, OptionsSnapshot, Ref, Value,
-};
+use crate::{Options, OptionsCache, OptionsFactory, OptionsMonitorCache, OptionsSnapshot, Ref, Value};
 
 /// Represents an object that manages [`Options`](crate::Options) and [option snapshots](crate::OptionsSnapshot).
 pub struct OptionsManager<T: Value> {
@@ -33,7 +31,6 @@ impl<T: Value> Options<T> for OptionsManager<T> {
 impl<T: Value> OptionsSnapshot<T> for OptionsManager<T> {
     #[inline]
     fn get(&self, name: Option<&str>) -> Ref<T> {
-        self.cache
-            .get_or_add(name, &|n| self.factory.create(n).unwrap())
+        self.cache.get_or_add(name, &|n| self.factory.create(n).unwrap())
     }
 }
