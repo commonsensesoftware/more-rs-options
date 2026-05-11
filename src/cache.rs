ad91cf471c7e1c40c2399a3f36dd1c929c0e6afd
Chris Martinez <sydefekt@hotmail.com>
2026-05-11 10:25:47 -0700

    Refactor to RwLock with double-read and without holding the lock during options creation

--- src/cache.rs
+++ src/cache.rs
@@ -1,9 +1,9 @@
 use crate::{validation::Error, Ref, Value};
 use std::collections::HashMap;
-use std::sync::Mutex;
+use std::sync::RwLock;
 
 /// Represents a monitored cache of configured options.
-pub struct Cache<T>(Mutex<HashMap<String, Ref<T>>>);
+pub struct Cache<T>(RwLock<HashMap<String, Ref<T>>>);
 
 impl<T> Default for Cache<T> {
     #[inline]
@@ -26,17 +26,23 @@ impl<T: Value> Cache<T> {
     /// * `name` - The optional name of the options
     /// * `create` - The function used to create options when added
     pub fn get_or_add(&self, name: &str, create: &dyn Fn(&str) -> Result<T, Error>) -> Result<Ref<T>, Error> {
-        let mut cache = self.0.lock().unwrap();
+        let cache = self.0.read().unwrap();
 
         if let Some(options) = cache.get(name) {
             return Ok(options.clone());
         }
 
+        drop(cache);
+
         let options = Ref::new(create(name)?);
+        let mut cache = self.0.write().unwrap();
 
-        cache.insert(name.to_owned(), options.clone());
-
-        Ok(options)
+        if let Some(options) = cache.get(name) {
+            Ok(options.clone())
+        } else {
+            cache.insert(name.to_owned(), options.clone());
+            Ok(options)
+        }
     }
 
     /// Attempts to add options with the specified name.
@@ -45,8 +51,13 @@ impl<T: Value> Cache<T> {
     ///
     /// * `name` - The optional name of the options
     /// * `options` - The options to add
+    ///
+    /// # Remarks
+    ///
+    /// Returns `true` if the options were added; otherwise, `false`. Options are not added if they already exist.
+    #[must_use]
     pub fn try_add(&self, name: &str, options: T) -> bool {
-        let mut cache = self.0.lock().unwrap();
+        let mut cache = self.0.write().unwrap();
 
         if cache.contains_key(name) {
             false
@@ -66,10 +77,11 @@ impl<T: Value> Cache<T> {
     ///
     /// Returns `true` if any options were removed; otherwise, `false`.
     pub fn remove(&self, name: &str) -> bool {
+        self.0.write().unwrap().remove(name).is_some()
     }
 
     /// Clears all options from the cache.
     pub fn clear(&self) {
-        self.0.lock().unwrap().clear()
+        self.0.write().unwrap().clear()
     }
 }
